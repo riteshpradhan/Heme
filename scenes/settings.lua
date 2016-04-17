8b761cba148d537b8e269e1718a511c2c5522584
Kush Chandra Shrestha <shrestha.kushchandra@gmail.com>
2016-04-16 22:15:07 -0500

    Added music and sound button on setting scene.

--- scenes/settings.lua
+++ scenes/settings.lua
@@ -1,32 +1,77 @@
 -- @Author: Kush Chandra Shrestha
 -- @Date:   2016-04-16 01:05:55
 -- @Last Modified by:   Kush Chandra Shrestha
--- @Last Modified time: 2016-04-16 15:45:28
+-- @Last Modified time: 2016-04-16 20:08:47
 
 -- load required library files
-local widget = require("widget")
-local composer = require( "composer" ); 
+local widget = require("widget");
+local composer = require("composer"); 
+local hemeDatabox = require('libs.databox')
+local utils = require("libs.utils");
 local scene = composer.newScene();
 
 function scene:create( event ) 
     local sceneGroup = self.view
-
     -- Load Background image
     local obox = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight); 
     obox:setFillColor(0,0,0,0.8);
     sceneGroup:insert(obox)
-
-    local is_playing = true
+    
+    local is_playing = false
     local x_offset, y_offset = 0, 0
+    
     if(is_playing) then
         x_offset = 400  
         y_offset = 230
     end
+
     local function btnBackHandler(event)
         local sceneOpt = {effect = "fade", time = 400}
         composer.gotoScene("scenes.menu", sceneOpt)
     end
 
+    local button_music, button_sound
+    local function btnMusicHandler(event)
+        button_music:removeSelf()
+        if(hemeDatabox.isMusicOn == true) then
+            hemeDatabox.isMusicOn = false
+            button_music = widget.newButton({
+                defaultFile = "images/menu/music_off.png",
+                onRelease = btnMusicHandler
+            })
+        else
+            hemeDatabox.isMusicOn = true
+            button_music = widget.newButton({
+                defaultFile = "images/menu/music_on.png",
+                onRelease = btnMusicHandler
+            })
+        end
+        button_music.x = display.contentCenterX - 100 + x_offset
+        button_music.y = display.contentCenterY + y_offset
+        sceneGroup:insert(button_music)
+    end
+
+    local function btnSoundHandler(event)
+        button_sound:removeSelf()
+        if(hemeDatabox.isSoundOn == true) then
+            hemeDatabox.isSoundOn = false
+            button_sound = widget.newButton({
+                defaultFile = "images/menu/sound_off.png",
+                onRelease = btnSoundHandler
+            })
+        else
+            hemeDatabox.isSoundOn = true
+            button_sound = widget.newButton({
+                defaultFile = "images/menu/sound_on.png",
+                onRelease = btnSoundHandler
+            })
+        end
+        button_sound.x = display.contentCenterX + 100 + x_offset
+        button_sound.y = display.contentCenterY + y_offset
+        sceneGroup:insert(button_sound)
+    end
+
+    
     if(is_playing) then
         local button_menu = widget.newButton({
             defaultFile = "images/menu/menu.png",
@@ -50,16 +95,33 @@ function scene:create( event )
         sceneGroup:insert(button_restart)
     end
 
-	local button_music = widget.newButton({
-        defaultFile = "images/menu/music_on.png",
-    })
+    if(hemeDatabox.isMusicOn == true) then
+        button_music = widget.newButton({
+            defaultFile = "images/menu/music_on.png",
+            onRelease = btnMusicHandler
+        })
+    else
+        button_music = widget.newButton({
+            defaultFile = "images/menu/music_off.png",
+            onRelease = btnMusicHandler
+        })
+    end
+	
     button_music.x = display.contentCenterX - 100 + x_offset
 	button_music.y = display.contentCenterY + y_offset
     sceneGroup:insert(button_music)
 
-    local button_sound = widget.newButton({
-        defaultFile = "images/menu/sound_on.png",
-    })
+    if(hemeDatabox.isSoundOn == true) then
+        button_sound = widget.newButton({
+            defaultFile = "images/menu/sound_on.png",
+            onRelease = btnSoundHandler
+        })
+    else
+        button_sound = widget.newButton({
+            defaultFile = "images/menu/sound_off.png",
+            onRelease = btnSoundHandler
+        })
+    end
     button_sound.x = display.contentCenterX + 100 + x_offset
     button_sound.y = display.contentCenterY + y_offset
     sceneGroup:insert(button_sound)
