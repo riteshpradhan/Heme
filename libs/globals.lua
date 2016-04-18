0f764cde94b591f19dd9b14072c934938c9c25a5
Ritesh Pradhan <ritesxz@gmail.com>
2016-04-18 02:03:39 -0500

    hemeglobals scrollspeed changed

--- libs/globals.lua
+++ libs/globals.lua
@@ -1,10 +1,10 @@
 -- @Author: Ritesh Pradhan
 -- @Date:   2016-04-10 21:20:16
 -- @Last Modified by:   Ritesh Pradhan
--- @Last Modified time: 2016-04-17 20:30:30
+-- @Last Modified time: 2016-04-18 01:54:27
 
 local _M = {}
 _M.yLevel = {[1]=260, [2]=160, [3]=60}
 _M.isGameOver = false
-_M.scrollSpeed = 10
+_M.scrollSpeed = 5
 return _M
