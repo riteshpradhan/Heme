74295b6c783cb1c42f7cf35b355a04395d2614ad
Ritesh Pradhan <ritesxz@gmail.com>
2016-04-17 22:06:21 -0500

    scroll speed and isGameOver added in globals

--- libs/globals.lua
+++ libs/globals.lua
@@ -1,8 +1,10 @@
 -- @Author: Ritesh Pradhan
 -- @Date:   2016-04-10 21:20:16
 -- @Last Modified by:   Ritesh Pradhan
--- @Last Modified time: 2016-04-10 21:28:19
+-- @Last Modified time: 2016-04-17 20:30:30
 
 local _M = {}
 _M.yLevel = {[1]=260, [2]=160, [3]=60}
+_M.isGameOver = false
+_M.scrollSpeed = 10
 return _M
