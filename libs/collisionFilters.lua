3dbeaf0b00f26026f1c76c60a27e0dea0ad63e7c
Ritesh Pradhan <ritesxz@gmail.com>
2016-04-20 01:07:55 -0500

    New Collision filter. Only player collects valuable things.

--- libs/collisionFilters.lua
+++ libs/collisionFilters.lua
@@ -1,17 +1,19 @@
 -- @Author: Ritesh Pradhan
 -- @Date:   2016-04-15 19:18:17
 -- @Last Modified by:   Ritesh Pradhan
--- @Last Modified time: 2016-04-17 15:41:00
+-- @Last Modified time: 2016-04-20 00:08:07
 
 
 CollisionFilters = {};
 CollisionFilters.enemyBullet= { categoryBits = 8, maskBits = 3};
-CollisionFilters.playerBullet= { categoryBits = 2, maskBits = 252};
+CollisionFilters.playerBullet= { categoryBits = 2, maskBits = 28};
 CollisionFilters.enemy= { categoryBits = 4, maskBits = 3};
 CollisionFilters.player= { categoryBits = 1, maskBits = 508};
-CollisionFilters.refill= { categoryBits = 32, maskBits = 3};
+CollisionFilters.refill= { categoryBits = 32, maskBits = 1};
 CollisionFilters.ground= { categoryBits = 256, maskBits = 1};
 CollisionFilters.obstruction= { categoryBits = 16, maskBits = 3};
-CollisionFilters.collectible= { categoryBits = 128, maskBits = 3};
-CollisionFilters.powerup= { categoryBits = 64, maskBits = 3};
+CollisionFilters.collectible= { categoryBits = 128, maskBits = 1};
+CollisionFilters.powerup= { categoryBits = 64, maskBits = 1};
 return CollisionFilters
+
+
