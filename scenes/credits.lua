21ce305fcf1cb29c6c2950b6e4d602ac723ee8ee
Ritesh Pradhan <ritesxz@gmail.com>
2016-04-20 21:41:59 -0500

    license changed. Credits updated.

--- scenes/credits.lua
+++ scenes/credits.lua
@@ -1,7 +1,7 @@
 -- @Author: Kush Chandra Shrestha
 -- @Date:   2016-04-15 23:45:03
 -- @Last Modified by:   Ritesh Pradhan
--- @Last Modified time: 2016-04-20 19:58:39
+-- @Last Modified time: 2016-04-20 21:06:41
 
 -- load required library files
 local widget = require("widget")
@@ -19,43 +19,56 @@ local scrollToTop = {}
 local scrollToBottom = {}
 
 
-local lotsOfText = [[This is
-                    Test
-                    Toast
-                    . No not transition.scaleTo( target, params )
-                    but scroll view it is.
-                    sdfa
-                    dfs
-                    fa
-                    dfsad
-                    fadf
-                    addEventListenerfa
-                    dfsadads
-                    f
-                    dfsadadsdf
-                    sdfasd
-                    function sdfasdfsd
-                        fsd
-                        f
-                        sdfasdfsdsd
-                        sdfasdfsdsdsd
-                        f
-                        sdfasdfsdsdsdsd
-                        fsd
-                        fsd
-                        f
-                        sdfasdfsdsdsdsdfds
-                        ( ... )
-                        -- body
-                    end]]
+local lotsOfText = [[
+
+
+
+HEME
+
+Developers
+1. Kush Chandra Shrestha
+2. Ritesh Pradhan
+3. Samyam Acharya
+
+
+Sound Effects
+1. http://soundbible.com
+2. http://opengameart.org/content/100-plus-game-sound-effects-wavoggm4a
+    Copyright/Attribution Notice:
+    you do not have to add a link but if you want to credit me feel free. Damaged Panda
+
+Background Music
+1. Menu Music:
+    ERH-BlueBeat-01-_loop_.wav
+    http://opengameart.org/content/bluebeat-01-loop-cyberpunk-lab-music
+    "BlueBeat 01" Copyright 2007 ERH http://www.freesound.org/people/ERH/ , License: http://creativecommons.org/licenses/by/3.0/
+
+2. Game Music:
+    beach.wav
+    http://opengameart.org/content/42-monster-rpg-2-music-tracks
+    These are the music files (in FLAC and OGG formats) from the recently open sourced Monster RPG 2. (Except jungle_ambience, which is not original).
+
+    The game is licensed under the Give It Your Own License, License, and I've chosen to re-license them under CC0, but I'm sure the good folks at Nooskewl Games would appreciate a shout out if you use their works.
+
+3. GameOver Music:
+    JordanTrudgett-battle-ccby3.wav
+    http://opengameart.org/content/fantasy-action-rpg-soundtrack-14-tracks-for-ardentryst-by-jordan-trudgett
+    Copyright 2008 Jordan Trudgett | http://jordan.trudgett.com/ - Licensed under CC BY 3.0 http://creativecommons.org/licenses/by/3.0/
+
+Images
+    1. Hema
+         Licensed under CC BY 3.0 http://creativecommons.org/licenses/by/3.0/
+
+    Copyright@2016 HEME
+]]
 
 
 function scrollToBottom(event)
-    scrollView:scrollTo( "bottom", { time=8000, onComplete=scrollToTop} )
+    scrollView:scrollTo( "bottom", {time=25000, onComplete=scrollToTop} )
 end
 
 function scrollToTop(event)
-    scrollView:scrollTo( "top", { time=100, onComplete=scrollToBottom } )
+    scrollView:scrollTo( "top", { time=10, onComplete=scrollToBottom } )
 end
 
 function scrollListener(event)
@@ -84,10 +97,10 @@ function scene:create( event )
 	end
 
     -- Load Background image
-    local bgImage = display.newImage("images/scenes/credits.png")
-    bgImage.x = display.contentCenterX
-	bgImage.y = display.contentCenterY
-    sceneGroup:insert(bgImage)
+ --    local bgImage = display.newImage("images/scenes/credits.png")
+ --    bgImage.x = display.contentCenterX
+	-- bgImage.y = display.contentCenterY
+ --    sceneGroup:insert(bgImage)
 
     local backButton = widget.newButton(
         {
@@ -100,7 +113,7 @@ function scene:create( event )
         }
     )
 
-    local caption = display.newText( sceneGroup, "CREDITS", display.contentCenterX, 100, "Comic Sans MS", 80 )
+    local caption = display.newText( sceneGroup, "CREDITS", display.contentCenterX, 75, "Comic Sans MS", 80 )
     caption:setFillColor( 0,0.5,0.3 )
 
     scrollView = widget.newScrollView
@@ -114,14 +127,14 @@ function scene:create( event )
         horizentalScrollDisabled = true,
         verticalScrollDisabled = false,
         listener = scrollListener,
-        backgroundColor = { 0.2, 0.2, 0.2, 0.6 },
+        backgroundColor = { 0.3, 0.3, 0.3, 0.6 },
     }
 
 
     local creditsText = display.newText( lotsOfText, 0, 0, 500, 0, "Comic Sans MS", 30 )
     creditsText:setFillColor( 1 )
     creditsText.x = display.contentCenterX
-    creditsText.y = display.contentCenterY + 200
+    creditsText.y = display.contentCenterY + 1000
     scrollView:insert( creditsText )
 
 
